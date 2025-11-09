a5572b51c8f4feab551accf96d03591ac79787df
mc02-GH <18305533222@163.com>
2025-11-09 17:17:07 +0800

    Add files via upload

---
+++ src/Csharp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Csapp;
+
+public static class Program
+{
+    public static void a()
+    {
+        Console.WriteLine("\nEnter string");
+        var x = Console.ReadLine();
+        string y;
+        while (true)
+        {
+            y = "  ";
+            Console.Write(y + x);
+            y = " ";
+            Console.Write(y + x);
+            y = "   ";
+            Console.Write(y + x);
+
+
+        }
+    }
+    public static void Main()
+    {
+        while (true)
+        {
+            a();
+        }
+
+    }
+}
