9b43d3f8e64f16f8a74daa620c06f1404bc9c85b
mc02-GH <18305533222@163.com>
2026-02-19 16:44:53 +0800

    Add hello function to print stylized greeting

---
+++ src/Csharp/HELLO/lib.cs
@@ -0,0 +1,19 @@
+using System;
+namespace Lib;
+internal static class print
+{
+    internal static void hello()
+    {
+        Console.Write("Ｈ　Ｈ　ＥＥＥ Ｌ　 　Ｌ　　　　Ｏ\n" +
+                      "Ｈ　Ｈ　Ｅ　　 Ｌ　 　Ｌ　　　Ｏ　Ｏ\n" +
+
+                      "Ｈ　Ｈ　ＥＥ　 Ｌ　 　Ｌ　　　Ｏ　Ｏ\n" +
+
+                      "ＨＨＨ　Ｅ　 　Ｌ　 　Ｌ　　　Ｏ　Ｏ\n" +
+
+                      "Ｈ　Ｈ　Ｅ　 　Ｌ　　 Ｌ　　　Ｏ　Ｏ\n" +
+
+                      "Ｈ　Ｈ　ＥＥＥ ＬＬＬ ＬＬＬ　　Ｏ\n");
+
+    }
+}
